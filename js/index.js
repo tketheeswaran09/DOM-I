5b74c93eb31bc8d1aa7e1b92bfb6eb26a872db0c
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-03-10 22:41:16 +0000

    Completed MVP!

--- js/index.js
+++ js/index.js
@@ -62,3 +62,55 @@ document.querySelector(".cta-text button").textContent =
 document
   .querySelector("#cta-img")
   .setAttribute("src", siteContent.cta["img-src"]);
+
+//MAIN CONTENT!
+document.querySelector(".text-content h4").textContent =
+  siteContent["main-content"]["features-h4"];
+document.querySelector(".text-content p").textContent =
+  siteContent["main-content"]["features-content"];
+
+textContentClassElements = document.getElementsByClassName("text-content");
+
+textContentClassElements[1].querySelector("h4").textContent =
+  siteContent["main-content"]["about-h4"];
+textContentClassElements[1].querySelector("p").textContent =
+  siteContent["main-content"]["about-content"];
+
+//MIDDLE IMAGE!
+
+let middleimg = document.getElementById(`middle-img`);
+middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//VISION!
+
+let visionH4 = document.getElementsByTagName('h4')[4];
+visionH4.innerText = "Vision";
+let visionContent = document.getElementsByTagName('p')[4];
+visionContent.innerText = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//PRODUCT!
+
+let productH4 = document.getElementsByTagName('h4')[3];
+productH4.innerText = "Product";
+let productContent = document.getElementsByTagName('p')[3];
+productContent.innerText = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//SERVICES!
+
+let servicesH4 = document.getElementsByTagName('h4')[2];
+servicesH4.innerText = "Services";
+let servicesContent = document.getElementsByTagName('p')[2];
+servicesContent.innerText = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//CONTACT!
+
+contactSectionElements = document.getElementsByClassName("contact")[0].children;
+contactSectionElements[0].textContent = siteContent["contact"]["contact-h4"];
+contactSectionElements[1].textContent = siteContent["contact"]["address"];
+contactSectionElements[2].textContent = siteContent["contact"]["phone"];
+contactSectionElements[2].textContent = siteContent["contact"]["email"];
+
+
+//FOOTER!
+
+document.querySelector("footer").textContent = siteContent.footer.copyright;
